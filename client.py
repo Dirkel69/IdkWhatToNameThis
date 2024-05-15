4236c96a5123e94f31e050ff3a2c39e62e4d780e
Gilad Hanania גלעד חנניה <133251295+Dirkel69@users.noreply.github.com>
2024-05-15 22:15:04 +0300

    Just added some code to client.py altho i dont think it all belongs there

--- client.py
+++ client.py
@@ -1,1 +1,23 @@
 import socket
+
+#preparing connection
+my_socket = socket.socket()
+IP_ADDRESS = socket.gethostbyname(socket.gethostname())
+PORT = 5050
+my_socket.connect(IP_ADDRESS, PORT)
+
+#Creating and sending message
+msg = "Hello, World!"
+encoded_msg = msg.encode()
+my_socket.send(encoded_msg)
+
+#Recieving and decoding the message
+encoded_data = my_socket.recv(1024)
+decoded_data = encoded_data.decode()
+print(f"The server sent: {decoded_data}")
+
+
+#Closing the socket
+my_socket.close()
+
+
